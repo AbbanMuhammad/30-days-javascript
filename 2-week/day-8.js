7c92797fa67a973717f9726f7cda606d3c33f8ac
Sani Ismail <saniismail2@gmail.com>
2026-01-25 20:24:52 +0100

    add challenge day 9.

--- 2-week/day-8.js
+++ 2-week/day-8.js
@@ -13,3 +13,53 @@ allItems.forEach(item => (item.innerText = "Updated item"));
 
 //Task 4:
 console.log(heading);
+
+// 1. Select the element
+const btn = document.querySelector("#btn");
+
+// 2. Add the Listener
+// Syntax: element.addEventListener("event_type", function_to_run);
+
+btn.addEventListener("click", () => {
+    console.log("Button was clicked!");
+    alert("You clicked me!");
+});
+
+btn.addEventListener("click", e => {
+    // 'e' stands for 'event object'. It contains details about the click.
+    // e.target refers to the specific element that was clicked.
+    e.target.innerText = "Clicked!";
+    e.target.style.backgroundColor = "green";
+});
+
+//Task 1:
+const myInput = document.querySelector("#myInput");
+const logBtn = document.querySelector("#logBtn");
+logBtn.addEventListener("click", () => {
+    console.log(myInput.value);
+});
+
+//Task 2:
+
+const myBody = document.body;
+const toggleBtn = document.querySelector("#toggleBtn");
+let isDackMode = false;
+toggleBtn.addEventListener("click", e => {
+    if ((isDackMode = !isDackMode || isDackMode === "")) {
+        myBody.style.backgroundColor = "black";
+        myBody.style.color = "white";
+    } else {
+        myBody.style.backgroundColor = "white";
+        myBody.style.color = "black";
+    }
+});
+
+//Task 3:
+const box = document.querySelector("#box");
+box.addEventListener("mouseenter", () => {
+    box.style.backgroundColor = "blue";
+});
+
+box.addEventListener("mouseleave", () => {
+    box.style.backgroundColor = "red";
+});
